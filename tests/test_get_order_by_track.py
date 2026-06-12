2750e25de48ee201e2246fcca1b0f3a6f23acc08
kuzka1287-hue <kuzka1287@gmail.com>
2026-06-12 22:05:33 +0300

    Update test_get_order_by_track.py

--- tests/test_get_order_by_track.py
+++ tests/test_get_order_by_track.py
@@ -1,8 +1,7 @@
-# test_get_order_by_track.py
+# test_get_order_by_track.py (дополнительное задание)
 import allure
-import pytest
 import requests
-from data import BASE_URL
+from urls import BASE_URL, TRACK_ORDER
 
 @allure.feature('Получение заказа по номеру')
 class TestGetOrderByTrack:
@@ -10,19 +9,22 @@ class TestGetOrderByTrack:
     @allure.title('Успешный запрос возвращает объект с заказом')
     def test_get_order_by_track_success(self, create_order):
         track = create_order()
-        response = requests.get(f'{BASE_URL}/orders/track?t={track}')
+        with allure.step(f"Получение заказа по треку {track}"):
+            response = requests.get(f'{BASE_URL}{TRACK_ORDER}', params={"t": track})
         assert response.status_code == 200
         assert "order" in response.json()
         assert response.json()["order"]["track"] == track
 
     @allure.title('Запрос без номера заказа возвращает ошибку')
     def test_get_order_no_track(self):
-        response = requests.get(f'{BASE_URL}/orders/track')
+        with allure.step("Запрос без параметра t"):
+            response = requests.get(f'{BASE_URL}{TRACK_ORDER}')
         assert response.status_code == 400
         assert response.json()["message"] == "Недостаточно данных для поиска"
 
     @allure.title('Запрос с несуществующим заказом возвращает ошибку')
     def test_get_order_invalid_track(self):
-        response = requests.get(f'{BASE_URL}/orders/track?t=999999999')
+        with allure.step("Запрос с несуществующим треком"):
+            response = requests.get(f'{BASE_URL}{TRACK_ORDER}', params={"t": 999999999})
         assert response.status_code == 404
         assert response.json()["message"] == "Заказ не найден"
