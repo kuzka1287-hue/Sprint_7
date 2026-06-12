49be2431bce1dd342e9a9148cae5bc9168e1bfa1
kuzka1287-hue <kuzka1287@gmail.com>
2026-06-12 21:55:41 +0300

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,7 @@
+# urls.py
+BASE_URL = 'https://qa-scooter.praktikum-services.ru/api/v1'
+COURIER = '/courier'
+LOGIN = '/courier/login'
+ORDERS = '/orders'
+ACCEPT_ORDER = '/orders/accept'
+TRACK_ORDER = '/orders/track'
