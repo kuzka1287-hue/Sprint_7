875e55f8e26ef1b20e93271790747b19a820698c
kuzka1287-hue <kuzka1287@gmail.com>
2026-06-12 21:56:27 +0300

    Update data.py

--- tests/data.py
+++ tests/data.py
@@ -1,24 +1,7 @@
 # data.py
 import random
 import string
-import requests
-
-BASE_URL = 'https://qa-scooter.praktikum-services.ru/api/v1'
 
 def generate_random_string(length=10):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for _ in range(length))
-
-def register_new_courier_and_return_login_password():
-    login = generate_random_string(10)
-    password = generate_random_string(10)
-    first_name = generate_random_string(10)
-    payload = {
-        "login": login,
-        "password": password,
-        "firstName": first_name
-    }
-    response = requests.post(f'{BASE_URL}/courier', data=payload)
-    if response.status_code == 201:
-        return login, password, first_name
-    return None
