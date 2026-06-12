b1634f1758ca0b8352f36112247e039695920743
kuzka1287-hue <kuzka1287@gmail.com>
2026-06-12 22:01:54 +0300

    Update test_orders_list.py

--- tests/test_orders_list.py
+++ tests/test_orders_list.py
@@ -1,14 +1,15 @@
 # test_orders_list.py
 import allure
 import requests
-from data import BASE_URL
+from urls import BASE_URL, ORDERS
 
 @allure.feature('Список заказов')
 class TestOrdersList:
 
     @allure.title('Тело ответа возвращает список заказов')
     def test_orders_list_returns_list(self):
-        response = requests.get(f'{BASE_URL}/orders')
+        with allure.step("Получение списка заказов"):
+            response = requests.get(f'{BASE_URL}{ORDERS}')
         assert response.status_code == 200
         assert isinstance(response.json(), dict)
         assert "orders" in response.json()
